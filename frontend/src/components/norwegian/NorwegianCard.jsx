41632b140e88f99d7bb35b7a1c061ffed01de1d7
Kajetan Pynka <siema1234@gazeta.pl>
2021-07-09 00:02:54 +0200

    Fixed responsiveness of the whole site (by appropriately scaling the tables)

--- frontend/src/components/norwegian/NorwegianCard.jsx
+++ frontend/src/components/norwegian/NorwegianCard.jsx
@@ -1,20 +1,45 @@
+import { useState, useEffect } from 'react';
+
 import Card from 'react-bootstrap/Card';
 import AdjGrammarNorwegian from './AdjGrammarNorwegian';
 import NounGrammarNorwegian from './NounGrammarNorwegian';
 import VerbGrammarNorwegian from './VerbGrammarNorwegian';
 
+import '../../styles/Card.css';
+
 function NorwegianCard(props) {
     let cardObj = props.cardObj;
     let singleTranslation = cardObj.translation[0];
 
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowWidth(window.innerWidth);
+        }
+
+        window.addEventListener('resize', handleResize);
+    });
+
     let result = null;
 
-    if ("grammarAdj" in singleTranslation) {
-        result = <AdjGrammarNorwegian grammarObj={singleTranslation.grammarAdj} />
-    } else if ("grammarNoun" in singleTranslation) {
-        result = <NounGrammarNorwegian grammarObj={singleTranslation.grammarNoun} />
-    } else if ("grammarVerb" in singleTranslation) {
-        result = <VerbGrammarNorwegian grammarObj={singleTranslation.grammarVerb} />
+    if (windowWidth >= 576) {
+        if ("grammarAdj" in singleTranslation) {
+            result = <AdjGrammarNorwegian grammarObj={singleTranslation.grammarAdj} />
+        } else if ("grammarNoun" in singleTranslation) {
+            result = <NounGrammarNorwegian grammarObj={singleTranslation.grammarNoun} />
+        } else if ("grammarVerb" in singleTranslation) {
+            result = <VerbGrammarNorwegian grammarObj={singleTranslation.grammarVerb} />
+        }
+    } else {
+        // TODO: make Grammars arranged in an order that will handle smaller devices
+        if ("grammarAdj" in singleTranslation) {
+            result = null;//<AdjGrammarNorwegian grammarObj={singleTranslation.grammarAdj} />
+        } else if ("grammarNoun" in singleTranslation) {
+            result = null;//<NounGrammarNorwegian grammarObj={singleTranslation.grammarNoun} />
+        } else if ("grammarVerb" in singleTranslation) {
+            result = null;//<VerbGrammarNorwegian grammarObj={singleTranslation.grammarVerb} />
+        }
     }
 
     let gender = "grammarNoun" in singleTranslation ? singleTranslation.grammarNoun.gender : null;
