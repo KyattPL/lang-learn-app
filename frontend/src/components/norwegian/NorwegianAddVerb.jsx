9194e57bdb78aa1d76edd4ea1a898e3f52d483b4
KyattPL <siema1234@gazeta.pl>
2021-08-15 13:34:06 +0200

    Fixes #8. If the request doesn't go through the proxy, the website won't seem to proceed with anything now. (Also, proxy errors are properly shown in the console)

--- frontend/src/components/norwegian/NorwegianAddVerb.jsx
+++ frontend/src/components/norwegian/NorwegianAddVerb.jsx
@@ -61,6 +61,8 @@ function NorwegianAddVerb({ wordSetter, showModal }) {
                     console.error(data);
                 } else if (data === "Internal Server Error") {
                     console.error("There was a problem with saving your card to the database");
+                } else if (data.startsWith("Proxy error:")){
+                    console.error(data);
                 } else  {
                     clearForm();
                     showModal();
