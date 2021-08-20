d62e1a4d2cc36249e6e73b63e13a03b2ebc58ed8
KyattPL <siema1234@gazeta.pl>
2021-08-20 17:56:29 +0200

    Something changed

--- frontend/src/components/AddCardAbstractForm.jsx
+++ frontend/src/components/AddCardAbstractForm.jsx
@@ -1,9 +1,18 @@
-// import DutchAddAbstract from './dutch/DutchAddAbstract.jxs';
+import { useRef, useState } from 'react';
 import NorwegianAddAbstract from './norwegian/NorwegianAddAbstract.jsx';
 
 function getFormStructure(lang, grammar) {
     const file = require(`../langJsons/${lang}.json`);
-    console.log(file[`add${grammar}`]);
+    const obj = file[`add${grammar}`]['translation'][0][`grammar${grammar}`]
+    if (obj) {
+        console.log(Object.values(obj));
+    }
+}
+
+function getGrammarInputNames(lang, grammar) {
+    const file = require(`../langJsons/${lang}.json`);
+    const obj = file[`add${grammar}`]['translation'][0][`grammar${grammar}`];
+    return Object.values(obj);
 }
 
 function AddCardAbstractForm({ langSelected, grammarSelected, wordSetter, showModal }) {
@@ -11,6 +20,16 @@ function AddCardAbstractForm({ langSelected, grammarSelected, wordSetter, showMo
     let result;
 
     getFormStructure(langSelected, grammarSelected);
+
+    const grammarInputNames = 0;
+    const grammarInputLen = grammarInputNames.length;
+
+    const [validated, setValidated] = useState(false);
+    const [checkedCountable, setCheckedCountable] = useState(false);
+
+    const wordInput = useRef(null);
+    const pronInput = useRef(null);
+    const meanInput = useRef(null);
 
     // grammarInputNames, grammarInputLen -> extract names from .json
     // dunno about word/pron/mean/gender Input refs
