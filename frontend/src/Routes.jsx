0b26642b7632b5bb5eb32776b0ac4d2035355886
Kajetan Pynka <siema1234@gazeta.pl>
2021-08-26 00:41:08 +0200

    Fixes #26. Added temporary buttons

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,9 +1,6 @@
 import React, { Suspense } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import CardDeck from 'react-bootstrap/CardDeck';
 
+import Homepage from './components/Homepage.jsx';
 import Header from './components/Header.jsx';
 import { Route, Switch } from 'react-router-dom';
 import './Routes.css';
@@ -18,29 +15,17 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/">
-          <Container fluid className="no-padding-container h-100">
-            <Row noGutters={true} className="content">
-              <Col>
-              </Col>
-              <Col xs="10">
-                <CardDeck className="deck">
-                  <h1>Hi there!</h1>
-                </CardDeck>
-              </Col>
-              <Col>
-              </Col>
-            </Row>
-          </Container>
+          <Homepage />
         </Route>
         <Route path="/addCard">
-            <Suspense fallback={<div></div>}>
-                <AddCard />
-            </Suspense>
+          <Suspense fallback={<div></div>}>
+            <AddCard />
+          </Suspense>
         </Route>
         <Route path="/searchCard">
-            <Suspense fallback={<div></div>}>
-                <SearchCard />
-            </Suspense>
+          <Suspense fallback={<div></div>}>
+            <SearchCard />
+          </Suspense>
         </Route>
       </Switch>
     </div>
